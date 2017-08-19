333e0e3e8325ba79b7f373993d5a04f49ac6c172
michelle61219 <michelle61219@gmail.com>
2017-08-18 23:10:13 -0600

    Added form tags and data binding

--- WebContent/WEB-INF/view/main-menu.jsp
+++ WebContent/WEB-INF/view/main-menu.jsp
@@ -4,5 +4,7 @@
 <h2>Spring MVC Demo - Home Page</h2>
 <hr>
 <a href ="hello/showForm"> Hello World form</a> <!-- hello/ : parent mapping for the controller  -->
+<br><br>
+<a href ="student/showForm"> Student form</a>
 </body>
 </html>
---
+++ src/com/luv2code/springdemo/mvc/StudentController.java
@@ -0,0 +1,36 @@
+package com.luv2code.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+	
+	// Create a method that shows forms
+	@RequestMapping("/showForm")
+	public String showForm(Model theModel) {
+		
+		// Create a student object
+		Student theStudent = new Student();
+		
+		// Add student object to the model
+		theModel.addAttribute("student",theStudent);
+		
+	
+		return "student-form";
+	}
+
+		@RequestMapping("/processForm")
+		public String processForm(@ModelAttribute("student") Student theStudent) {					
+			
+			//Log the input data
+			System.out.println("theStudent: " + theStudent.getFirstName() + " "
+					+ theStudent.getLastName());
+			
+			return "student-confirmation";
+		}
+
+}
